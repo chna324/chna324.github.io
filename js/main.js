dc8ba8c1464613b2ade2ab02bccfd55feb9057da
chna324 <chna324@gmail.com>
2022-05-30 17:17:33 +0900

    btn 스크립트 추가

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-$('.rnd_slider').bxSlider({
-    pager:false,
-    minSlides:3,
-    moveSlides:1,
-    slideWidth:965,
-    slideMargin:24
-});
+$(function(){
 
-//비디오 모달
-let videoBtn = $('.video_icon'),
-    targetVideo = $('#video');
-
-    videoBtn.click(function(e){
+    //rnd 멀티플 슬라이더
+    $('.rnd_slider').bxSlider({
+        pager:false,
+        minSlides:3,
+        moveSlides:1,
+        slideWidth:965,
+        slideMargin:24
+    });
+    
+    //비디오 모달
+    let videoBtn = $('.video_icon'),
+        targetVideo = $('#video');
+    
+        videoBtn.click(function(e){
+            e.preventDefault();
+            targetVideo.addClass('active');
+        });
+        targetVideo.click(function(){
+            targetVideo.removeClass('active');
+        });
+    
+    //버튼
+    let Btn = $('.fixed');
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 600){
+            Btn.addClass('active');
+        }else{
+            Btn.removeClass('active');
+        }
+    });
+    Btn.click(function(e){
         e.preventDefault();
-        targetVideo.addClass('active');
+       $('body,html').animate({scrollTop:0},500);
     });
-    targetVideo.click(function(){
-        targetVideo.removeClass('active');
-    });
+});
